f6b04ddf506c9c04c33366490c0b765b9d25b603
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-12-15 20:02:04 +0700

    Menambahkan fungsi Select, Delete, dan Update didalam DAO

--- app/src/main/java/com/example/p7mvvmdb/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/p7mvvmdb/data/dao/MahasiswaDao.kt
@@ -1,15 +1,24 @@
 package com.example.p7mvvmdb.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.p7mvvmdb.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
-    @Query("select * from mahasiswa")
+    @Query("select * from mahasiswa ORDER BY nama ASC")
     fun getAllMahasiswa() : Flow<List<Mahasiswa>>
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    @Query("SELECT * FROM mahasiswa where nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
 }
